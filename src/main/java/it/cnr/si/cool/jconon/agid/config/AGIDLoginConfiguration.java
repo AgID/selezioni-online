0c48b84f7a37dbc1a830013cd38cfdd749cccc88
marco.spasiano <marco.spasiano@cnr.it>
2020-12-14 11:37:08 +0100

    Configuration of AGID Login

--- src/main/java/it/cnr/si/cool/jconon/agid/config/AGIDLoginConfiguration.java
+++ src/main/java/it/cnr/si/cool/jconon/agid/config/AGIDLoginConfiguration.java
@@ -1,17 +1,22 @@
 package it.cnr.si.cool.jconon.agid.config;
 
+import feign.Client;
 import feign.Feign;
 import feign.auth.BasicAuthRequestInterceptor;
 import feign.form.FormEncoder;
 import feign.gson.GsonDecoder;
 import feign.gson.GsonEncoder;
 import it.cnr.si.cool.jconon.agid.repository.AGIDLogin;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.ssl.SSLContexts;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
 
 @Configuration
 @ConditionalOnProperty(name = "agid-login.url")
@@ -33,6 +38,20 @@ public class AGIDLoginConfiguration {
                 .decoder(new GsonDecoder())
                 .encoder(new FormEncoder(new GsonEncoder()))
                 .target(AGIDLogin.class, properties.getUrl());
+    }
+
+    @Bean
+    public Client feignClient() {
+        return new Client.Default(getSSLSocketFactory(), new NoopHostnameVerifier());
+    }
+
+    private SSLSocketFactory getSSLSocketFactory() {
+        try {
+            SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build();
+            return sslContext.getSocketFactory();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
 }
